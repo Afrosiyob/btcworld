ae4d3f065574547db44b4eba8d6bca8701e3d4ea
fmustafoyeva2004@gmail.com <12022004FsS$>
2021-02-05 22:23:59 +0500

    adminka

--- src/pages/admin/statistics/Statistics.js
+++ src/pages/admin/statistics/Statistics.js
@@ -2,47 +2,50 @@ import React from "react";
 import { Col, Row } from "antd";
 import StatisticCard from "./StatisticCard/StatisticCard";
 import StatisticChart from "./StatisticChart/StatisticChart";
+import { useTranslation } from "react-i18next";
 
 function Statistics() {
+  const { t } =useTranslation();
+  
   return (
     <div>
-      <h2 className="w-100 text-muted mb-3">Total in team</h2>
+      <h2 className="w-100 text-muted mb-3">{t(`Statistics.Titul`)}</h2>
 
       <Row gutter={[15, 15]}>
         <Col sm={12} md={8}>
           <StatisticCard
-            bgColor={"#1CC8EE"}
-            bgIcon={"rgba(255,255,255,0.37)"}
-            bgShadow={"#ABF0FF"}
+            bgColor={"white"}
+            bgIcon={"rgb(222, 211, 255)"}
+            bgShadow={"rgba(255, 255, 255, 0.5)"}
             iconClass={"fas fa-users"}
-            iconColor={"white"}
-            titul={"4"}
-            subTitul={"Personally invited"}
-            textColor={"white"}
+            iconColor={"rgb(95, 46, 234)"}
+            titul={"8"}
+            subTitul={t(`Statistics.Titul2`)}
+            textColor={"black"}
           />
         </Col>
         <Col sm={12} md={8}>
           <StatisticCard
-            bgColor={"#1CC8EE"}
-            bgIcon={"rgba(255,255,255,0.37)"}
-            bgShadow={"#ABF0FF"}
-            iconClass={"fas fa-users"}
-            iconColor={"white"}
-            titul={"4"}
-            subTitul={"Personally invited"}
-            textColor={"white"}
+            bgColor={"white"}
+            bgIcon={"rgb(222, 211, 255)"}
+            bgShadow={"rgba(255, 255, 255, 0.5)"}
+            iconClass={"fas fa-hand-holding-usd"}
+            iconColor={"rgb(95, 46, 234)"}
+            titul={"0 USD"}
+            subTitul={t(`Statistics.Titul3`)}
+            textColor={"black"}
           />
         </Col>
         <Col sm={12} md={8}>
           <StatisticCard
-            bgColor={"#1CC8EE"}
-            bgIcon={"rgba(255,255,255,0.37)"}
-            bgShadow={"#ABF0FF"}
-            iconClass={"fas fa-users"}
-            iconColor={"white"}
-            titul={"4"}
-            subTitul={"Personally invited"}
-            textColor={"white"}
+            bgColor={"white"}
+            bgIcon={"rgb(222, 211, 255)"}
+            bgShadow={"rgba(255, 255, 255, 0.5)"}
+            iconClass={"fas fa-hand-holding-heart"}
+            iconColor={"rgb(95, 46, 234)"}
+            titul={"0 USD"}
+            subTitul={t(`Statistics.Titul4`)}
+            textColor={"black"}
           />
         </Col>
       </Row>
@@ -53,7 +56,7 @@ function Statistics() {
         </Col>
         <Col sm={12} md={8}>
           <div className="mb-4">
-            <h4 className="w-100 text-muted mb-3">Total in team</h4>
+            <h4 className="w-100 text-muted mb-3">{t(`Statistics.Titul`)}</h4>
             <StatisticCard
               bgColor={"#1CC8EE"}
               bgIcon={"rgba(255,255,255,0.37)"}
@@ -61,7 +64,7 @@ function Statistics() {
               iconClass={"fas fa-users"}
               iconColor={"white"}
               titul={"4"}
-              subTitul={"Personally invited"}
+              subTitul={t(`Statistics.Titul5`)}
               textColor={"white"}
             />
           </div>
@@ -74,7 +77,7 @@ function Statistics() {
               iconClass={"fas fa-level-down-alt"}
               iconColor={"white"}
               titul={"0.00 USD"}
-              subTitul={"Total"}
+              subTitul={t(`Statistics.Titul6`)}
               textColor={"white"}
             />
           </div>

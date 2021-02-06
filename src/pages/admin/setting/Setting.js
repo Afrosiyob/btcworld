144dd2321010d4281c14300c6537a49cf967da50
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-02-06 23:12:58 +0500

    add
    add

--- src/pages/admin/setting/Setting.js
+++ src/pages/admin/setting/Setting.js
@@ -1,7 +1,30 @@
+import { Card, Col, Descriptions, Image, Row } from "antd";
 import React from "react";
 
 function Setting() {
-  return <div>this is setting</div>;
+  return (
+    <div>
+      <h2 className="w-100 text-muted mb-3">Products</h2>
+
+      <Row gutter={[8, 8]}>
+        <Col sm={24} md={6}>
+          <h4 className="w-100 text-muted mb-3">Basic info</h4>
+          <Card>
+            <Image src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
+            <Descriptions title="User Info" layout="vertical" bordered>
+              <Descriptions.Item label="Product">
+                Cloud Database
+              </Descriptions.Item>
+            </Descriptions>
+          </Card>
+        </Col>
+        <Col sm={24} md={18}>
+          <h4 className="w-100 text-muted mb-3">Changing</h4>
+          <Card>wefew</Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Setting;

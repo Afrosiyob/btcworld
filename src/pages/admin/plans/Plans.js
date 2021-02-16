e99e724403322def490c8590872ddaf5b2e39994
fmustafoyeva2004@gmail.com <12022004FsS$>
2021-02-17 00:01:01 +0500

    ta'rif

--- src/pages/admin/plans/Plans.js
+++ src/pages/admin/plans/Plans.js
@@ -1,13 +1,13 @@
-import { Button, Result } from "antd";
+import { Card, Col, Row } from "antd";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import HelmetTitle from "../../../components/Halmet/HelmetTitle";
 import { getPlans } from "../../../store/plans/action";
 
 function Plans() {
-  let history = useHistory();
+  // let history = useHistory();
 
   const dispatch = useDispatch();
 
@@ -17,18 +17,75 @@ function Plans() {
 
   const { t } = useTranslation();
   return (
-    <div>
+    <div className="plans-style">
       <HelmetTitle title={t(`Admin.Titul5`)} />
-      <Result
+
+      <Row
+        gutter={[8, 8]}
+        className="mb-3"
+        justify="space-between"
+        align="middle"
+      >
+        <Col>
+          <h2 className="w-100 text-muted mb-3">{t(`Admin.Titul5`)}</h2>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6}>
+          <Card className="shadow-sm text-center rounded" headStyle={{ backgroundColor: '#002140',  textTransform: "uppercase", color: 'white' }}  title="Ta'rif 1" bordered={false}>
+            <h2>
+              100000 <small>so'm</small>
+            </h2>
+            <p>~ birinchi ta'rif haqida</p>
+            <button className="header-btn rounded-pill px-5">
+              Sotib olish
+              </button>
+          </Card>
+        </Col>
+        <Col span={6}>
+        <Card className="shadow-sm text-center rounded" headStyle={{ backgroundColor: '#002140',  textTransform: "uppercase", color: 'white' }}  title="Ta'rif 1" bordered={false}>
+            <h2>
+              100000 <small>so'm</small>
+            </h2>
+            <p>~ birinchi ta'rif haqida</p>
+            <button className="header-btn rounded-pill px-5">
+              Sotib olish
+              </button>
+          </Card>
+        </Col>
+        <Col span={6}>
+        <Card className="shadow-sm text-center rounded" headStyle={{ backgroundColor: '#002140',  textTransform: "uppercase", color: 'white' }}  title="Ta'rif 1" bordered={false}>
+            <h2>
+              100000 <small>so'm</small>
+            </h2>
+            <p>~ birinchi ta'rif haqida</p>
+            <button className="header-btn rounded-pill px-5">
+              Sotib olish
+              </button>
+          </Card>
+        </Col>
+        <Col span={6}>
+        <Card className="shadow-sm text-center rounded" headStyle={{ backgroundColor: '#002140',  textTransform: "uppercase", color: 'white' }}  title="Ta'rif 1" bordered={false}>
+            <h2>
+              100000 <small>so'm</small>
+            </h2>
+            <p>~ birinchi ta'rif haqida</p>
+            <button className="header-btn rounded-pill px-5">
+              Sotib olish
+              </button>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* <Result
         status="404"
-        // title="404"
         subTitle={t(`plans.Titul`)}
         extra={
           <Button onClick={() => history.goBack()} type="primary">
             {t(`plans.Titul2`)}
           </Button>
         }
-      />
+      /> */}
     </div>
   );
 }
